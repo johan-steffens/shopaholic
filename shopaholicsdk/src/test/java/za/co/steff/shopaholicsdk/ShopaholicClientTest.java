6aa574b43b34ce9539f6ef09c464af6b15a62ac3
Johan Steffens <johan@angleorange.com>
2019-07-26 10:32:21 +0200

    Loading of external test data. Added 'Run Tests' to shared run configurations

--- shopaholicsdk/src/test/java/za/co/steff/shopaholicsdk/ShopaholicClientTest.java
+++ shopaholicsdk/src/test/java/za/co/steff/shopaholicsdk/ShopaholicClientTest.java
@@ -2,6 +2,8 @@ package za.co.steff.shopaholicsdk;
 
 import android.util.Log;
 
+import com.google.gson.Gson;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,13 +13,19 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import za.co.steff.shopaholicsdk.network.model.CitiesResponse;
 import za.co.steff.shopaholicsdk.network.service.MockShopaholicService;
 import za.co.steff.shopaholicsdk.network.service.ShopaholicService;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(PowerMockRunner.class)
@@ -27,15 +35,26 @@ public class ShopaholicClientTest {
     @Mock
     ShopaholicService shopaholicService;
 
+    private CitiesResponse expectedResponse;
+
     @Before
     public void setUp() {
         // Tell PowerMockito to mock all static calls of the Log class
         // Without this any static Log calls will throw an exception
         PowerMockito.mockStatic(Log.class);
+
+        // Load our expected response from a json file
+        InputStream successResponse = getClass().getClassLoader().getResourceAsStream("success-response.json");
+        InputStreamReader streamReader = new InputStreamReader(successResponse);
+        expectedResponse = new Gson().fromJson(streamReader, CitiesResponse.class);
+
+        // Assert that our test data loaded successfully
+        assertNotNull(expectedResponse);
+        assertNotEquals(expectedResponse.getCities().size(), 0);
     }
 
     @Test
-    public void clientLoadsSuccessfully() throws Exception {
+    public void clientLoadSucceeds() throws Exception {
         // Create a mock ShopaholicService that returns success and an empty body
         shopaholicService = new MockShopaholicService(true);
 
@@ -65,7 +84,7 @@ public class ShopaholicClientTest {
     }
 
     @Test
-    public void clientFailsGracefully() throws Exception {
+    public void clientLoadFailsGracefully() throws Exception {
         // Replace our existing ShopaholicService with one that expects an initialization error
         shopaholicService = new MockShopaholicService(false);
 
@@ -92,6 +111,11 @@ public class ShopaholicClientTest {
 
         // When our latch releases, assert that our client loaded without issues
         assertFalse(initializationResult.get());
+    }
+
+    @Test
+    public void clientReturnsListOfCities() {
+
     }
 
     @After
